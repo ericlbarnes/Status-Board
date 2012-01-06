929ade6ba250bb0474332bf2eea72d17ec9d7db6
Eric Famiglietti <eric.famiglietti@gmail.com>
2012-01-05 22:47:03 -0500

    Add widget refresh functionality.

--- public/js/core.js
+++ public/js/core.js
@@ -1,30 +1,48 @@
-$('section').each(function(index) {
-	load_widget(this);
-});
+$(document).ready(function() {
 
-function load_widget(widget){
-	$(widget).html('<div class="loading"><img src="'+BASE_URL+'/themes/wood/ajax-loader-large.gif"></div>').fadeIn();
-
-	var data = $(widget).data();
-	var name = $(widget).attr("data-widget");
-
-	// This is nasty but couldn't think
-	// of a better way at this time.
-	var a = [];
-	for (key in data) {
-		a.push(key+"="+data[key]);
-	}
-	a.push("size="+$(widget).attr('class'));
-	var serialized = a.join("&");
-
-	$.ajax({
-		data: serialized,
-		dataType: "html",
-		type: "POST",
-		url: SITE_URL+name+"?"+new Date().getTime(),
-		success: function(data) {
-			$(widget).html(data).fadeIn();
-		}
+	$('section').each(function(index) {
+		init_widget(this);
 	});
-	return false;
-}
+
+	function init_widget(widget) {
+		var name = $(widget).attr("data-widget");
+		var data = $(widget).data();
+
+		// This is nasty but couldn't think
+		// of a better way at this time.
+		var a = [];
+		for (key in data) {
+			a.push(key+"="+data[key]);
+		}
+		a.push("size="+$(widget).attr('class'));
+		var serialized = a.join("&");
+
+		// Initial loading of widget
+		load_widget(widget, name, serialized);
+
+		// Set timer to refresh widget
+		var interval = $(widget).attr('data-interval');
+		if (interval) {
+			setInterval(function() {
+				load_widget(widget, name, serialized);
+			}, interval * 1000);
+		}
+
+		return false;
+	}
+
+	function load_widget(widget, name, data) {
+		$(widget).html('<div class="loading"><img src="'+BASE_URL+'/themes/wood/ajax-loader-large.gif"></div>').fadeIn();
+
+		$.ajax({
+			data: data,
+			dataType: "html",
+			type: "POST",
+			url: SITE_URL+name+"?"+new Date().getTime(),
+			success: function(data) {
+				$(widget).html(data).fadeIn();
+			}
+		});
+		return false;
+	}
+});
