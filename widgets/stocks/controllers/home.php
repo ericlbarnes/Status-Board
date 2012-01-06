87392c3e27430404cd38e92fa22a7b6806b06cc6
Eric Famiglietti <eric.famiglietti@gmail.com>
2012-01-05 19:49:44 -0500

    Remove RESTful nature of controller.

--- widgets/stocks/controllers/home.php
+++ widgets/stocks/controllers/home.php
@@ -2,28 +2,7 @@
 
 class Stocks_Home_Controller extends Controller {
 
-	public $restful = true;
-
-	public function get_index()
-	{
-		$view_file = 'stocks::'.Input::get('size', 'small');
-		$exchange = 'NASDAQ';
-		$symbol = 'GOOG';
-
-		$stock_data = $this->_get_data($exchange, $symbol);
-
-		$view = View::make($view_file)
-			->with('exchange', $exchange)
-			->with('symbol', $symbol)
-			->with('price', $stock_data[0]['l'])
-			->with('change', $stock_data[0]['c'])
-			->with('change_class', $stock_data[0]['c'] > 0 ? 'up' : 'down')
-			->with('time', $stock_data[0]['lt'])
-			->with('css', File::get(BUNDLE_PATH.'stocks/stocks.css'));
-		exit($view);
-	}
-
-	public function post_index()
+	public function action_index()
 	{
 		$view_file = 'stocks::'.Input::get('size', 'small');
 		$exchange = Input::get('exchange', 'NASDAQ');
@@ -45,7 +24,7 @@ class Stocks_Home_Controller extends Controller {
 	private function _get_data($exchange, $symbol)
 	{
 		$url = "http://finance.google.com/finance/info?client=ig&q={$exchange}:{$symbol}";
-		
+
 		$quote = file_get_contents($url);
 
 		// Remove CR's from ouput - make it one line
