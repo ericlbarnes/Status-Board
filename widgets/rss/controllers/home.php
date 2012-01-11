1f7f4828f39d82bef407757549a2df5460c19c58
Jeff Madsen <jrmadsen67@gmail.com>
2012-01-11 17:40:31 +0900

    update rss widget to use config file

--- widgets/rss/controllers/home.php
+++ widgets/rss/controllers/home.php
@@ -10,15 +10,28 @@ class Rss_Home_Controller extends Controller {
 	public function post_index()
 	{
 		$view_file = 'rss::'.Input::get('size', 'small');
+		$config_key = Input::get('config');
 		
-		$site = Input::get('site', 'The Onion');
-		$rss_feed = $this->_get_rss_feed(Input::get('url', 'http://feeds.theonion.com/theonion/daily'), Input::get('feedcount', 5));
+		if (!empty($config_key))
+		{
+			$settings = Config::get('widgets.'.$config_key);
+			if (!empty($settings)) {
 
+          		$site = $settings['site'];
+          		$rss_feed = $this->_get_rss_feed($settings['url'], $settings['feedcount']);			
+			}
+			else {
+				$rss_feed = "No available settings";
+			}	
+
+
+		}
 		$view = View::make($view_file)
 			->with('rss_feed', $rss_feed)
 			->with('site', $site)
 			->with('css', File::get(BUNDLE_PATH.'rss/rss.css'));
-		exit($view);
+		exit($view);	
+
 	}
 
 	private function _get_rss_feed($url, $feedcount)
